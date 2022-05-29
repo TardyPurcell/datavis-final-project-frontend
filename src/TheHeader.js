29b7ab3a13d767687a8ed2edb231340f7a76a5dc
Tardy Purcell <tardypurcell@outlook.com>
2022-05-30 01:27:27 +0800

    ...

---
+++ src/TheHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header, Icon } from 'semantic-ui-react'
+
+const TheHeader = () => (
+  <Header as='h2'>
+    <Icon name='chart pie' />
+    <Header.Content>
+      歌词文本数据可视化
+      <Header.Subheader>迟云帆 唐明珑</Header.Subheader>
+    </Header.Content>
+  </Header>
+)
+
+export default TheHeader
